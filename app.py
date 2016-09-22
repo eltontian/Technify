3a0cc541e8a1546d9cc4803661bf2defcfcac1e3
Elton Tian <eltontian2@gmail.com>
2016-09-22 19:13:21 -0400

    Pasting in favor of blending

--- app.py
+++ app.py
@@ -12,44 +12,17 @@ ALLOWED_IMAGE_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 def process_image(img):
   #open up the mask
-  mask = Image.open('./filters/BigLittleFilter.png')
-  mask = mask.convert('RGBA')
-  #make sure it matches the size of the image
-  mask = mask.resize(img.size)
-  #enhance it by raising its saturation
-  converter = ImageEnhance.Color(mask)
-  mask = converter.enhance(2.0)
-  #get mutable data
-  mdata = mask.getdata()
+  overlay = Image.open('./filters/BigLittleFilter.png')
 
-  #make sure our image has alpha channel
+  #make sure our images have alpha channel
+  overlay = overlay.convert('RGBA')
   img = img.convert('RGBA')
-  #dummy convert does nothing but available for saturation tweaking
-  converter = ImageEnhance.Color(img)
-  img = converter.enhance(1.0)
-  #get mutable data
-  idata = img.getdata()
 
-  #create output data
-  newdata = []
-  #loop through our mask and image pixels
-  for iitem, mitem in zip(idata, mdata):
-    #ratio of each pixel is 1:2 (image:mask)
-    #tweak this for different colorings
-    im = 1.0
-    mm = 2.0
-    #the higher b, the higher the brightness
-    b = .3
-    #create RGBs
-    r = int((iitem[0] * im + mitem[0] * mm) / (im + mm - b))
-    g = int((iitem[1] * im + mitem[1] * mm) / (im + mm - b))
-    b = int((iitem[2] * im + mitem[2] * mm) / (im + mm - b))
-    a = 255
-    #add it to our output
-    newdata.append((r, g, b, a))
+  #make sure it matches the size of the image
+  overlay = overlay.resize(img.size)
 
-  #create an image from our new combined data
-  img.putdata(newdata)
+  img.paste(overlay, (0, 0), overlay)
+
   #unique name
   filename = uuid.uuid4().hex + '.png'
   filename = os.path.join('/tmp', filename)
